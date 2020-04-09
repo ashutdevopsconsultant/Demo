5e30fcdf74fa37c0df08297de00d48d99fc94403
ashutdevopsconsultant <63342979+ashutdevopsconsultant@users.noreply.github.com>
2020-04-09 17:21:48 +0530

    InitialCodeUpgate

---
+++ sboot-demo/src/main/java/com/demo2000/HelloWorldsbootApp.java
@@ -0,0 +1,12 @@
+package com.demo2000;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldsbootApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldsbootApp.class, args);
+	}
+}
